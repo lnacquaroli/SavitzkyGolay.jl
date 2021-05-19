5398df828d33697d1ca3f57605f10c22ef9e0b32
Leandro Acquaroli <26485264+lnacquaroli@users.noreply.github.com>
2021-05-19 14:57:26 -0400

    Add files via upload

--- examples/readme_examples.jl
+++ examples/readme_examples.jl
@@ -1,12 +1,30 @@
-using SavitzkyGolay
-using Plots
+
+cd(raw"C:\Users\flvisa\Desktop\lna\numerical_analysis\github\savitzky_golay")
+
+include("SavitzkyGolay.jl")
+import .SavitzkyGolay as sgolay
+import Plots as plt
+# plt.pyplot()
 
 t = 0:20
 y = collect(0:20)
-sg = savitzky_golay(y, 11, 2)
-plot(t, [y y_.y], label=["Original signal" "Filtered signal"], ylabel="", xlabel="t", legend=:topleft)
+y_ = sgolay.savitzky_golay(y, 11, 2)
+plt.plot(t, [y y_.y], label=["Original signal" "Filtered signal"], ylabel="", xlabel="t", legend=:topleft)
 
 t = LinRange(-4, 4, 500)
 y = exp.(-t.^2) .+ 0.05 .* (1.0 .+ randn(length(t)))
-sg = savitzky_golay(y, 21, 4)
-plot(t, [y sg.y], label=["Original signal" "Filtered signal"], ylabel="", xlabel="t", legend=:topleft)
+y_ = sgolay.savitzky_golay(y, 21, 4)
+plt.plot(t, [y y_.y], label=["Original signal" "Filtered signal"], ylabel="", xlabel="t", legend=:topleft)
+
+# With constructor
+t = 0:20
+y = collect(0:20)
+f1 = sgolay.SGolay(w=11, order=2)
+y1 = f1(y)
+plt.plot(t, [y y1.y], label=["Original signal" "Filtered signal"], ylabel="", xlabel="t", legend=:topleft)
+
+t = LinRange(-4, 4, 500)
+y = exp.(-t.^2) .+ 0.05 .* (1.0 .+ randn(length(t)))
+f2 = sgolay.SGolay(w=21, order=4)
+y2 = f2(y)
+plt.plot(t, [y y2.y], label=["Original signal" "Filtered signal"], ylabel="", xlabel="t", legend=:topleft)
